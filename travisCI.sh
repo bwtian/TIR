fff46d669c39bee5d4434587961f07e594230dc9
Bingwei Tian <bwtian@gmail.com>
2014-09-14 17:12:07 +0900

    - ‘.#travisCI.sh’ / ‘travisCI.sh’

--- travisCI.sh
+++ travisCI.sh
@@ -1,4 +1,4 @@
-git checkout -b travis &&
+git checkout -b travis && 
 wget https://github.com/craigcitro/r-travis/raw/master/sample.travis.yml -O .travis.yml &&
 sed -i -e '$a\' .Rbuildignore &&
 echo '^\.travis\.yml$' >> .Rbuildignore &&
