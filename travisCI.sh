82d8a2db6f5a646146135a4e73dc343acdf65aba
Bingwei Tian <bwtian@gmail.com>
2014-09-14 17:11:54 +0900

    + ‘travisCI.sh’

---
+++ travisCI.sh
@@ -0,0 +1,7 @@
+git checkout -b travis &&
+wget https://github.com/craigcitro/r-travis/raw/master/sample.travis.yml -O .travis.yml &&
+sed -i -e '$a\' .Rbuildignore &&
+echo '^\.travis\.yml$' >> .Rbuildignore &&
+git add .travis.yml .Rbuildignore &&
+git commit -m "enable continuous integration via craigcitro/r-travis" &&
+git push origin travis
