8f6d5db02aedae9169036af17bf416fa4b4a20e6
t3500 <bwtian@gmail.com>
2014-09-16 15:52:12 +0900

    - ‘R/cptar.R’ - ‘R/untar.R’

--- R/untar.R
+++
@@ -1,5 +0,0 @@
-
-rs.untar  <- function(path = ".", outdir = "../L8_tifs", pattern = ".tgz") {
-        tarFiles_v <- list.files(path = path, pattern = pattern, all.files = T, full.names = T) 
-        sapply(tarFiles_v, function(i) {untar(tarfile = i, exdir = file.path(outdir,tools::file_path_sans_ext(basename(i))))})
-}
