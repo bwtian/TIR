c7c5cf700d2d3dc3d0d2294348a96e58868b76db
t3500 <bwtian@gmail.com>
2014-09-16 17:44:25 +0900

    / ‘R/rs.cptar.R’ - ‘paper/.#TIR.org’ / ‘paper/TIR.org’

--- R/rs.cptar.R
+++ R/rs.cptar.R
@@ -1,10 +1,10 @@
 
-#' Copy files in the list from fromdir to todir
+#' Copy files in the list from fromdir to todir, untar and delete tarballs
 #' @param list LC81050292013262LGN00.tgz
 #' @param formdir folders contain the Image usrally the download or database
 #' @param todir  Where the files will copy to, usually the workdir
-
     rs.cptar <- function(list, fromdir, todir){
+            if
             if (all(list %in% dir(fromdir))){
                     for (i in list){
                             file.copy(file.path(fromdir,i), todir)
