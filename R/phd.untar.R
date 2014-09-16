0214a11ed0d3f891b15228fae1088b66b28e9e23
t3500 <bwtian@gmail.com>
2014-09-16 18:02:58 +0900

    / ‘R/phd.untar.R’ + ‘R/rs.readL8.R’ + ‘R/rs.todown.R’

--- R/phd.untar.R
+++ R/phd.untar.R
@@ -1,5 +1,5 @@
 
 rs.untar  <- function(list, path = ".", outdir = "../tif", pattern = ".tgz") {
         tarFiles_v <- list.files(path = path, pattern = pattern, all.files = T, full.names = T) 
-        sapply(tarFiles_v, function(i) {untar(tarfile = i, exdir = file.path(outdir, tools::file_path_sans_ext(basename(i))))})
+        sapply(tarFiles_v, function(i) {utils::untar(tarfile = i, exdir = file.path(outdir, tools::file_path_sans_ext(basename(i))))})
 }
