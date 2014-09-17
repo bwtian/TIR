23858f99c25b87cb1d945f1a4ce01736780b18ce
Bingwei Tian <bwtian@gmail.com>
2014-09-17 09:36:12 +0900

    + ‘R/l8.DNtoTOARadiance.R’ - ‘R/rs.cptar.R’ / ‘R/rs.readL8.R’ / ‘R/rs.untar.R’ < ‘R/phd.untar.R’ > ‘R/rs.untar1.R’ + ‘paper/.#TIR.org’ / ‘paper/TIR.org’ + ‘paper/auto/TIR.el’

--- R/rs.untar.R
+++ R/rs.untar.R
@@ -1,5 +1,15 @@
 
-rs.untar  <- function(list, outdir = "../tif", pattern = ".tgz") {
-        
-        sapply(list, function(i) {untar(tarfile = i, exdir = file.path(outdir, tools::file_path_sans_ext(basename(i))))})
+#' Select and untar files in the list form tar dir to tif dir 
+#' @param list like LC81050292013262LGN00.tgz or LC81050292013262LGN00.tar.gz
+#' @param formdir folders contain the Image usrally the download or database
+#' @param todir  Where the files will copy to, usually the workdir
+rs.untar <- function(list, fromdir, todir){
+        if (all(list %in% dir(fromdir))){
+                sapply(list, function(i) {
+                        sceneDir <-file.path(tools::file_path_as_absolute(todir), gsub(pattern = "(^[^.]+)(.*)", replacement = "\\1", i))
+                        utils::untar(tarfile = file.path(tools::file_path_as_absolute(fromdir),i), exdir = sceneDir)
+                })
+          } else {
+                  stop (paste("Not all the files in", fromdir))
+          }
 }
