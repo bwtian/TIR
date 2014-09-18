f72260039f600c804bf42137a83a69909969e03d
Bingwei Tian <bwtian@gmail.com>
2014-09-18 15:14:16 +0900

    + ‘demo/rs10_DNtoTOARadiance.R’ - ‘paper/.#TIR.org’ / ‘paper/TIR.org’

---
+++ demo/rs10_DNtoTOARadiance.R
@@ -0,0 +1,31 @@
+
+sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
+sourceDir("~/SparkleShare/TIR/R/")
+worklist  <- paste0(hkdMinCloud$ID,".tgz")
+dir.tif  <- "~/Share500sda/Landsat8/at0_Sensor"
+dir.toaRad  <- "~/Share500sda/Landsat8/at1_TOA/toaRad"
+library(raster)
+setwd(dir.tif)  ## very important tips for use rLandsat8
+## files  <- sapply(file.path(dir.tif,list.files(dir.tif)), tools::file_path_as_absolute)
+## basename(files)
+## l8.lst  <- lapply(basename(files), ReadLandsat8)
+l8.lst   <- lapply(dir(dir.tif), ReadLandsat8)
+bandnames <-c("aerosol", "blue", "green", "red",
+              "nir", "swir1", "swir2",
+              "panchromatic",
+              "cirrus",
+              "tirs1", "tirs2")
+for (i in l8.lst) {
+        sceneName  <- i$metadata$landsat_scene_id
+        if (!file.exists(file.path(dir.toaRad, sceneName))) {
+                dir.create(file.path(dir.toaRad, sceneName), recursive = T)
+        }
+        for(j in bandnames){
+                idx <- seq_along(bandnames)[sapply(bandnames, function(x) j %in% x)] # a number
+                bandidx <- paste0("file_name_band_", idx)
+                bandName <-  sapply(i, "[[", bandidx)[[1]]
+                Rad.rst  <- ToTOARadiance(i, j)
+                writeRaster(Rad.rst, filename = file.path(dir.toaRad, sceneName, bandName), overwrite = T)
+                raster::removeTmpFiles(h = 0.03) ## Improtant tips for save hardisk
+        }
+}
