f29394feedb95c969e9c32069b146aa6e65e1fe9
Bingwei Tian <bwtian@gmail.com>
2014-09-18 14:49:45 +0900

    + ‘R/rs.readL8.R’ + ‘R/rs.todown.R’

---
+++ R/rs.todown.R
@@ -0,0 +1,8 @@
+
+rs.todown <- function(list, downeddir, tolistName){
+        downedFiles  <- list.files(downeddir)
+        todownList   <-  setdiff(list, downedFiles)
+        Now <- format(Sys.time(), "_%y%m%d_%H%M%S")
+        todownListName <- paste0(todownListName, Now, ".txt")
+        write.table(todownList , todownListName, quote = F, row.names = F,col.names = F)
+}
