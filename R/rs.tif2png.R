1a33c0ed00994b8e343a46daa2010e74561be2f4
Bingwei Tian <bwtian@gmail.com>
2014-09-21 14:37:18 +0900

    / ‘R/rs.tif2png.R’ - ‘paper/.#TIR.org’ / ‘paper/TIR.org’

--- R/rs.tif2png.R
+++ R/rs.tif2png.R
@@ -2,7 +2,8 @@
 rs.tif2png <- function(inRaster){
         for (i in inRaster) {
                 outName <- gsub("\\.tif", "\\.tif.png", i)
-                pict <- raster::spplot(i, col.regions = rainbow(200, start = 2/6, end = 1))
+                r <- raster(i)
+                pict <- raster::spplot(r, col.regions = rainbow(200, start = 2/6, end = 1))
                 png(filename = outName)
                 print(pict)
                 dev.off()
