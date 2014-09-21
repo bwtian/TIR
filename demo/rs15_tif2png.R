8de3c70530b228f031bb3221aa54e869c3d43784
Bingwei Tian <bwtian@gmail.com>
2014-09-21 14:27:14 +0900

    / ‘demo/rs15_tif2png.R’ - ‘paper/.#TIR.org’ / ‘paper/TIR.org’

--- demo/rs15_tif2png.R
+++ demo/rs15_tif2png.R
@@ -3,5 +3,6 @@ source("~/SparkleShare/Rprofile/R/sourceDir.R")
 sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
 sourceDir("~/SparkleShare/TIR/R/")
 dataDir <- "~/Share500sda/Landsat8/at1_TOA/toaBT"
+setwd(dataDir)
 inRaster <- list.files(dataDir, pattern = ".tif$", recursive = TRUE)
 rs.tif2png(inRaster)
