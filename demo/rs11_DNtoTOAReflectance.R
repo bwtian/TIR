34b5fe0c516864c32f168e54cda82815936cd0b6
Bingwei Tian <bwtian@gmail.com>
2014-09-24 22:46:45 +0900

    + ‘demo/rs11_DNtoTOAReflectance.R’ + ‘demo/rs12_DNtoTOAReflectanceSun.R’ / ‘demo/rs15_tif2png.R’

---
+++ demo/rs11_DNtoTOAReflectance.R
@@ -0,0 +1,34 @@
+
+source("~/SparkleShare/Rprofile/R/sourceDir.R")
+sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
+sourceDir("~/SparkleShare/TIR/R/")
+dir.tif  <- "~/Share500sda/Landsat8/at0_Sensor"
+dir.toaRef  <- "~/Share500sda/Landsat8/at1_TOA/toaRef"
+dir.tmp <- "~/Share500sda/Landsat8/raster_tmp"
+library(raster)
+rasterOptions(tmpdir = dir.tmp)  
+
+setwd(dir.tif)  ## very important tips for use rLandsat8
+## files  <- sapply(file.path(dir.tif,list.files(dir.tif)), tools::file_path_as_absolute)
+## basename(files)
+## l8.lst  <- lapply(basename(files), ReadLandsat8)
+l8.lst   <- lapply(dir(dir.tif), ReadLandsat8)
+bandnames <-c("aerosol", "blue", "green", "red",
+              "nir", "swir1", "swir2",
+              "panchromatic",
+              "cirrus")
+for (i in l8.lst) {
+        sceneName  <- i$metadata$landsat_scene_id
+        if (!file.exists(file.path(dir.toaRef, sceneName))) {
+                dir.create(file.path(dir.toaRef, sceneName), recursive = T)
+        }
+        for(j in bandnames){
+                idx <- seq_along(bandnames)[sapply(bandnames, function(x) j %in% x)] # a number
+                bandidx <- paste0("file_name_band_", idx)
+                bandName <-  sapply(i, "[[", bandidx)[[1]]
+                # fileName <- paste0(tools::file_path_sans_ext(bandName), "_TOARef.tif")
+                Ref.rst  <- ToTOAReflectance(i, j)
+                writeRaster(Ref.rst, filename = file.path(dir.toaRef, sceneName, bandName), overwrite = T)
+                raster::removeTmpFiles(h = 0.5) ## Improtant tips for save hardisk
+        }
+}
--- demo/rs15_tif2png.R
+++ demo/rs15_tif2png.R
@@ -2,7 +2,6 @@
 source("~/SparkleShare/Rprofile/R/sourceDir.R")
 sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
 sourceDir("~/SparkleShare/TIR/R/")
-dataDir <- "~/Share500sda/Landsat8/at1_TOA/toaBT"
+dataDir <- "~/Share500sda/Landsat8/at1_TOA"
 setwd(dataDir)
-inRaster <- list.files(dataDir, pattern = ".tif$", recursive = TRUE)
-rs.tif2png(inRaster)
+rs.tif2png()
