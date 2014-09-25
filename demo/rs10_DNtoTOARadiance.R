0df0e3269dffb21285e88ac789c389696049dfc8
t3500 <bwtian@gmail.com>
2014-09-25 16:04:07 +0900

    / ‘demo/rs10_DNtoTOARadiance.R’ / ‘note/TIR.org’

--- demo/rs10_DNtoTOARadiance.R
+++ demo/rs10_DNtoTOARadiance.R
@@ -37,7 +37,7 @@ for (i in l8.lst) {
                 plot(Rad.rst)
                 title(main = paste("Radiance at TOA of Band ", idx))
                 require("grid")
-                grid.text(expression(paste("[", W*sr^-1*m^-2*mu*m^-1,"]")), x=unit(0.98, "npc"), y=unit(0.50, "npc"), rot=-90)
+                grid.text(expression(paste("[", W*sr^-1*m^-2*mu*m^-1,"]")), x=unit(0.97, "npc"), y=unit(0.50, "npc"), rot=-90)
                 dev.off()
                 raster::removeTmpFiles(h = 1) ## Improtant tips for save hardisk
         }
