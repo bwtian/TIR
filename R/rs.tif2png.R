be36782eb3664e9571f46479950a429858e61c77
lab1 <bwtian@gmail.com>
2014-09-25 10:38:51 +0900

    / ‘R/rs.tif2png.R’

--- R/rs.tif2png.R
+++ R/rs.tif2png.R
@@ -2,10 +2,11 @@
 rs.tif2png <- function(dir = getwd()){
         inRaster <- list.files(path = dir, pattern = ".tif$", full.names = TRUE, recursive = TRUE)
         for (i in inRaster) {
+                require(raster)
                 outName <- gsub("\\.tif", "\\.tif.png", i)
                 r <- raster(i)
                 png(filename = outName)
-                raster::plot(r)
+                plot(r)
                 dev.off()
         }
 }
