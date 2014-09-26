b578edbe513ba114a27bbe0d050ba407c1bf2d6b
t3500 <bwtian@gmail.com>
2014-09-26 16:21:00 +0900

    / ‘demo/rs17_reporjectTif.R’

--- demo/rs17_reporjectTif.R
+++ demo/rs17_reporjectTif.R
@@ -7,7 +7,10 @@ library(sp)
 library(rgdal)
 library(raster)
 toCRS  <- sp::CRS(lccWgs84)
-
+source("~/SparkleShare/Rprofile/R/sourceDir.R")
+sourceDir("~/SparkleShare/Rprofile/R/")
+dir.tmp <- "~/Share500sda/Landsat8/raster_tmp"
+rasterOptions(tmpdir = dir.tmp)
 dir.toaTbK  <- "~/Share500sda/Landsat8/at1_TOA/toaTbK/"
 dir.toaTbKlcc  <-  "~/Share500sda/Landsat8/at1_TOA/toaTbKlcc"
 if (!file.exists(dir.toaTbKlcc)){
