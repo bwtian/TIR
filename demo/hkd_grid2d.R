36226b15707cb0dfdde8435e9b3a9bb7882b77be
t3500 <bwtian@gmail.com>
2014-09-26 17:59:57 +0900

    / ‘demo/hkd_grid2d.R’

--- demo/hkd_grid2d.R
+++ demo/hkd_grid2d.R
@@ -1,14 +1,14 @@
 #' Make 2D 100m * 100m Grid with lccWgs84 CRS
-#' 
+#'
 library(sp)
-library(gstat)  
+library(gstat)
 library(plyr)
 library(rgdal)
 library(maptools)
 library(rgeos)
 
 
-## BBOX
+## BBOX Raster
 P  <- Polygon(cbind(c(1152500, 1152500, 1673500, 1673500, 1152500),
                     c(1372500, 1843500, 1843500, 1372500, 1372500)))
 Ps  <- Polygons(list(P), "P")
@@ -16,5 +16,11 @@ SpP  <- SpatialPolygons(list(Ps))
 bbgrid1h  <- spsample(SpP,type = "regular", cellsize = c(100,100),
                       offset = c(0.5, 0.5))
 proj4string(bbgrid1h) <- CRS(lccWgs84)
+bbraster1h  <- rasterFromXYZ(bbgrid1h)
+bbraster1h[]  <- 1: ncell(bbraster1h)
+proj4string(bbraster1h) <- CRS(lccWgs84)
+proj4string(bbraster1h)
+saveRDS(bbraster1h, file = "~/SparkleShare/TIR/hkdbb_grdi2d1h.Rds")
+plot(bbraster1h)
 
-## Hokkaido Shape
+## Hokkaido Shape Raster
