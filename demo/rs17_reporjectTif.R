b266ab8e84cafa73cf750758dc99740e5e90de85
t3500 <bwtian@gmail.com>
2014-09-26 15:34:03 +0900

    - ‘demo/.#rs17_reporjectTif.R’ / ‘demo/rs17_reporjectTif.R’

--- demo/rs17_reporjectTif.R
+++ demo/rs17_reporjectTif.R
@@ -4,3 +4,9 @@
 #' @param tif a list of tif files
 
 dir.toaTbK  <- "~/Share500sda/Landsat8/at1_TOA/toaTbK/"
+tif <- list.files(path= dir.toaTbK ,
+                  pattern= ".tif$",
+                  all.files=TRUE,
+                  full.names=TRUE,
+                  recursive=TRUE,
+                  ignore.case=TRUE)
