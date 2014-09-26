ab0ec6b28168379d7a2ce4a2fbb30520ee899217
t3500 <bwtian@gmail.com>
2014-09-26 18:19:04 +0900

    / ‘demo/rs17_reporjectTif.R’

--- demo/rs17_reporjectTif.R
+++ demo/rs17_reporjectTif.R
@@ -28,7 +28,8 @@ r.rst  <- lapply(tif, raster)
 hkd1h  <- readRDS("~/SparkleShare/TIR/hkdbb_grdi2d1h.Rds")
 for (i in r.rst) {
         outName  <- paste0(names(i), ".tif")
-        projectRaster(from = i, to = hkd1h,
+        #projectRaster(from = i, crs = toCRS,  method = "ngb",
+        projectRaster(from = i,  to = hkd1h,
                       filename =  file.path(dir.toaTbKlcc, outName),
                       overwrite=TRUE)
         raster::removeTmpFiles(h = 1) ## Improtant tips for save hardisk
