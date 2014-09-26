8e6e02080fc9f9845b7b138178c0b587c0bded98
t3500 <bwtian@gmail.com>
2014-09-26 18:08:42 +0900

    / ‘demo/hkd_grid2d.R’

--- demo/hkd_grid2d.R
+++ demo/hkd_grid2d.R
@@ -9,8 +9,10 @@ library(rgeos)
 
 
 ## BBOX Raster
-P  <- Polygon(cbind(c(1152500, 1152500, 1673500, 1673500, 1152500),
-                    c(1372500, 1843500, 1843500, 1372500, 1372500)))
+# P  <- Polygon(cbind(c(1152500, 1152500, 1673500, 1673500, 1152500),
+#                     c(1372500, 1843500, 1843500, 1372500, 1372500)))
+P  <- Polygon(cbind(c(1152550, 1152550, 1673550, 1673550, 1152550),
+                    c(1372550, 1843550, 1843550, 1372550, 1372550)))
 Ps  <- Polygons(list(P), "P")
 SpP  <- SpatialPolygons(list(Ps))
 bbgrid1h  <- spsample(SpP,type = "regular", cellsize = c(100,100),
@@ -20,7 +22,16 @@ bbraster1h  <- rasterFromXYZ(bbgrid1h)
 bbraster1h[]  <- 1: ncell(bbraster1h)
 proj4string(bbraster1h) <- CRS(lccWgs84)
 proj4string(bbraster1h)
-saveRDS(bbraster1h, file = "~/SparkleShare/TIR/hkdbb_grdi2d1h.Rds")
-plot(bbraster1h)
 
+# plot(bbraster1h)
+# saveRDS(bbraster1h, file = "~/SparkleShare/TIR/hkdbb_grdi2d1h.Rds")
+hkd1h  <- readRDS("~/SparkleShare/TIR/hkdbb_grdi2d1h.Rds")
+proj4string(hkd1h)
 ## Hokkaido Shape Raster
+
+shp  <- "~/Dropbox/2data/dataRaw/japan_ver71/HokkaidoUnion_lccWgs84.shp"
+
+hkdshp  <- readShapePoly(shp)
+
+plot(hkd1h)
+plot(hkdshp, add = T)
