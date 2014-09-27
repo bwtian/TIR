ff2329cefb4f1cf9efb559e665e2b58f85be7f6c
Bingwei Tian <bwtian@gmail.com>
2014-09-27 11:58:13 +0900

    / ‘demo/rs18_scaleTif.R’

--- demo/rs18_scaleTif.R
+++ demo/rs18_scaleTif.R
@@ -26,7 +26,7 @@ tif <- list.files(path= dir.toaTbKlcc,
 r.rst  <- lapply(tif, raster)
 hkdmaskb  <- readRDS("~/SparkleShare/TIR/hkdmskb_grdi2d1h.Rds")
 r.rstm  <- lapply(r.rst, function(x) mask(x, hkdmaskb))
-for (i in r.rst) {
+for (i in r.rstm) {
         outName  <- paste0(names(i), ".tif")
         #projectRaster(from = i, crs = toCRS,  method = "ngb",
         zscore  <- raster::scale(i)
