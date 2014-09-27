daf4cfb544593d07decf79b68ac0a90771508c5c
Bingwei Tian <bwtian@gmail.com>
2014-09-27 21:29:55 +0900

    / ‘demo/emissivity.R’

--- demo/emissivity.R
+++ demo/emissivity.R
@@ -1,6 +1,5 @@
 library(raster)
 library(sp)
-library(ggplot2)
 
 source("~/SparkleShare/Rprofile/R/sourceDir.R")
 sourceDir("~/SparkleShare/Rprofile/R/")
@@ -15,54 +14,19 @@ projectRaster(from = hkdLulc,  to = hkdmaskb,
               overwrite=TRUE)
 raster::removeTmpFiles(h = 1)
 ### Reclassify("is", "become")
-emi  <- raster("hkdLulc100lcc.tif")
+lulc  <- raster("hkdLulc100lcc.tif")
 m  <- c(1, 0.95, # Rice paddy
         2, 0.96, # Farm land
         5, 0.97, # Forest
         6, 0.93, # Vacant land
         7, 0.94, # Buildings
         9, 0.92, # Roads
-        10, 0.95 # Other lands
-        11, 0.99 # Inland water
-        14, 0.96 # Seashore
-        15, 0.99 # Ocean water
-        16, 0.97 # Golf Courses
+        10, 0.95, # Other lands
+        11, 0.99, # Inland water
+        14, 0.96, # Seashore
+        15, 0.99, # Ocean water
+        16, 0.97, # Golf Courses
         17, 0.95 # Railway
         )
-rclmat  <- matrix(m, ncol=3, byrow=TRUE)
-
-p$emi[p$emi == 1]  <- 0.95  # Rice paddy
-p$emi[p$emi == 2]  <- 0.96  # Farm land
-p$emi[p$emi == 5]  <- 0.97  # Forest
-p$emi[p$emi == 6]  <- 0.93  # Vacant land
-p$emi[p$emi == 7]  <- 0.94  # Buildings
-p$emi[p$emi == 9]  <- 0.92  # Road
-p$emi[p$emi == 10]  <- 0.95 # Other lands
-p$emi[p$emi == 11]  <- 0.99 # Inland water
-p$emi[p$emi == 14]  <- 0.96 # Seashore
-p$emi[p$emi == 15]  <- 0.99 # Ocean water
-p$emi[p$emi == 16]  <- 0.97 # Golf Courses
-p$emi[p$emi == 17]  <- 0.95 # Railway
-
-
-writeRaster(pp, filename="emissivity.tif", format="GTiff", overwrite=TRUE)
-
-lst  <- b/(1 + (10.8 * b / 14380) * ln(a))
-
-
-
-point_r[merge = 1]  <- 1
-writeRaster()
-summary(point_r)
-point_r
-plot(point_r)
-class(point_r)
-
-class(lucc)
-class(point)
-
-head(point_m)
-summary(point_m)
-plot(lucc)
-help.search("point")
-lucc_r
+rclmat  <- matrix(m, ncol=2, byrow=TRUE)
+emi <- reclassify(lulc, rclmat, filename = "hkdEmissivity.tif")
