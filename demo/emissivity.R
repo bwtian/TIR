2354a4d6790debf11f97ca44f10581c7e6c061db
lab1 <bwtian@gmail.com>
2014-09-27 21:39:56 +0900

    / ‘demo/emissivity.R’

--- demo/emissivity.R
+++ demo/emissivity.R
@@ -30,7 +30,7 @@ m  <- c(1, 0.95, # Rice paddy
         17, 0.95 # Railway
         )
 
-rclmat  <- matrix(m, ncol=2, byrow=TRUE)  <-
+rclmat <- matrix(m, ncol=2, byrow=TRUE)
 now <- format(Sys.time(), "_%y%m%d_%H%M%S")
 emiName  <- paste0("hkdEmissivity", now, ".tif")
 hkdEmi <- reclassify(hkdlulc, rclmat, filename = emiName)
