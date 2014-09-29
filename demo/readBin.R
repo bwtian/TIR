bed866bd35132347448bc98b6a6047c1664de11d
Bingwei Tian <bwtian@gmail.com>
2014-09-29 23:37:19 +0900

    / ‘demo/readBin.R’

--- demo/readBin.R
+++ demo/readBin.R
@@ -1,7 +1,8 @@
 #' Read NASA ASTER Global Emissivity Database (ASTER GED) Binary Datasets
 #'
 #'
-#'
+#' @details
+#' @author Bingwei Tian
 #'
 #' @param emiB10m  layer1 is  Emissivity mean of Band10 scaled by 1000
 #' @param emiB11m  layer2 is  Emissivity mean of Band11 scaled by 1000
@@ -25,13 +26,6 @@
 
 
 
-
-
-
-
-
-
-
 toRead  <- file("~/ASTB/AG100B.v003.43.142.0001.bin", "rb")
 layers  <- 19
 res  <- 100
@@ -40,32 +34,5 @@ data.v  <- readBin(toRead, integer(), size = 4, n = dim)
 close(toRead)
 
 layer.l <-  split(data.v, ceiling(seq_along(data.v)/1000000))
-str(layer.l)
-n  <- seq_along(data.v)
-max(n)
-ceiling(max(n)/1000000)
-###Method2
-str(layer.l)
-splitAt <- function(x, pos) unname(split(x, cumsum(seq_along(x) %in% pos)))
-splitAt2 <- function(x, pos) {
-        out <- list()
-        pos2 <- c(1, pos, length(x)+1)
-        for (i in seq_along(pos2[-1])) {
-                out[[i]] <- x[pos2[i]:(pos2[i+1]-1)]
-        }
-        return(out)
-}
-splitAt2()
-function(x, pos) {pos <- c(1L, pos, length(x) + 1L); Map(function(x, i, j) x[i:j], list(x), head(pos, -1L), tail(pos, -1L) - 1L)}
-qs <- quantile(seqa, seq(0, 1, length.out =20))
-y <- cut(x, round(qs), include.lowest = TRUE)
-split(1:1900, 1:100)
-splitAt <- function(x, pos) unname(split(x, cumsum(seq_along(x) %in% pos)))
-splitAt(1:1900, 1:10*10)
-cut(1:12, 2)
-d  <- 1:1900
-ceiling(seq_along(d)/100)
-s  <- split(d, ceiling(seq_along(d)/100))
-class(s)
-split(1:1900, )
-ceiling(0.2)
+
+layer.df  <- as.matr(layer.l)
