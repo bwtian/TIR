0cbd56b2601f71cb68ac6820a1f8c456863fe67f
t3500 <bwtian@gmail.com>
2014-09-30 14:50:33 +0900

    / ‘R/rs91_readAG100B.R’

--- R/rs91_readAG100B.R
+++ R/rs91_readAG100B.R
@@ -1,6 +1,6 @@
 source("~/SparkleShare/Rprofile/R/sourceDir.R")
 sourceDir("~/SparkleShare/Rprofile/R/")
-sourceDir("~/SparkleShare/Rprofile/R/")
+sourceDir("~/SparkleShare/TIR/R/")
 
 dir.tmp <- "~/Share500sda/Landsat8/raster_tmp"
 rasterOptions(tmpdir = dir.tmp)
