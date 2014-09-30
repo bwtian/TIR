dc4fb1419117a4d6557e6df397fb0eb5c699d222
t3500 <bwtian@gmail.com>
2014-09-30 14:57:32 +0900

    / ‘R/rs91_readAG100B.R’

--- R/rs91_readAG100B.R
+++ R/rs91_readAG100B.R
@@ -4,5 +4,21 @@ sourceDir("~/SparkleShare/TIR/R/")
 
 dir.tmp <- "~/Share500sda/Landsat8/raster_tmp"
 rasterOptions(tmpdir = dir.tmp)
-dir.toaTbK  <- "~/Share500sda/Landsat8/at1_TOA/toaTbK/"
-dir.toaTbKlcc  <-  "~/Share500sda/Landsat8/at1_TOA/toaTbKlcc"
+
+dir.AG100B  <- "~/Share500sda/AG100B/"
+
+### Hokkaido Area
+bins.l <- list.files(path=dir.AG100B,
+                   pattern="bin$",
+                   all.files=TRUE,
+                   #full.names=TRUE,
+                   recursive=TRUE,
+                   ignore.case=TRUE)
+bins.df <- as.data.frame(cbind(bins.l, do.call(rbind, strsplit(bins.l, "[.]"))))
+colnames(bins.df)  <- c("ID", "Product", "Ver", "Lat", "Lon", "Num", "Format")
+bins.df[4:5]  <-  sapply(bins.df[, 4:5], as.numeric)
+
+hkd  <- subset(bins.df, Lon >= 139 & Lon <=146 & Lat <=46 )
+plot(hkd$Lon, hkd$Lat)
+hkd.l  <- hkd[[1]]
+hkdAG100B.l  <- readAG100B(hkd.l)
