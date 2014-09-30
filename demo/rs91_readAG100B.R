0646c95789604cb36e26875f2dd9223f851f81e9
lab1 <bwtian@gmail.com>
2014-09-30 18:09:39 +0900

    / ‘demo/rs91_readAG100B.R’

--- demo/rs91_readAG100B.R
+++ demo/rs91_readAG100B.R
@@ -11,12 +11,12 @@ bins.l <- list.files(path=dir.AG100B,
                    #full.names=TRUE,
                    recursive=TRUE,
                    ignore.case=TRUE)
-bins.df <- as.data.frame(cbind(bins.l, do.call(rbind, strsplit(bins.l, "[.]"))))
+bins.df <- as.data.frame(cbind(bins.l, do.call(rbind, strsplit(bins.l, "[.]"))), stringsAsFactors = FALSE)
 colnames(bins.df)  <- c("ID", "Product", "Ver", "Lat", "Lon", "Num", "Format")
-bins.df[4:5]  <-  sapply(bins.df[, 4:5], as.numeric)
-
+bins.df[,4]  <- as.numeric(bins.df[,4])
+bins.df[,5]  <- as.numeric(bins.df[,5])
 hkd  <- subset(bins.df, Lon >= 139 & Lon <=146 & Lat <=46 )
-plot(hkd$Lon, hkd$Lat)
+#plot(hkd$Lon, hkd$Lat)
 hkd.l  <- hkd[[1]]
 hkdAG100B.l   <- readAG100B(hkd.l)
 hkdAG100B.df  <- do.call(rbind, hkdAG100B.l)
