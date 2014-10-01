0578c8dd94bf9e667884222c5b23634163673da3
Bingwei Tian <bwtian@gmail.com>
2014-10-01 21:57:18 +0900

    / ‘demo/tirSettings.R’

--- demo/tirSettings.R
+++ demo/tirSettings.R
@@ -29,6 +29,7 @@ dir.toaTbK <- file.path(dir.toa, "toaTbK")
 dir.toaTbC <- file.path(dir.toa, "toaTbC")
 dir.toaTbKE <- file.path(dir.toa, "toaTbKE")
 dir.toaEmi  <-   file.path(dir.toa, "toaEmi")
+dir.toaTbKlcc  <-  file.path(dir.toa,"toaTbKlcc")
 
 ###  Options
 dir.tmp    <- file.path(driver, "raster_tmp")
