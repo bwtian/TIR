121630908974b2f77e83d63b9319880ceab97264
Bingwei Tian <bwtian@gmail.com>
2014-10-01 21:42:17 +0900

    / ‘demo/rs11_DNtoTOAReflectance.R’

--- demo/rs11_DNtoTOAReflectance.R
+++ demo/rs11_DNtoTOAReflectance.R
@@ -1,17 +1,5 @@
-
-source("~/SparkleShare/Rprofile/R/sourceDir.R")
-sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
-sourceDir("~/SparkleShare/TIR/R/")
-dir.tif  <- "~/Share500sda/Landsat8/at0_Sensor"
-dir.toaRef  <- "~/Share500sda/Landsat8/at1_TOA/toaRef"
-dir.tmp <- "~/Share500sda/Landsat8/raster_tmp"
-library(raster)
-rasterOptions(tmpdir = dir.tmp)  
-
+source("./tirSettings.R")
 setwd(dir.tif)  ## very important tips for use rLandsat8
-## files  <- sapply(file.path(dir.tif,list.files(dir.tif)), tools::file_path_as_absolute)
-## basename(files)
-## l8.lst  <- lapply(basename(files), ReadLandsat8)
 l8.lst   <- lapply(dir(dir.tif), ReadLandsat8)
 bandnames <-c("aerosol", "blue", "green", "red",
               "nir", "swir1", "swir2",
