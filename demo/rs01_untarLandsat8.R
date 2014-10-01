3cb59391b4ff2188fea085a65750f1fed1a04a75
Bingwei Tian <bwtian@gmail.com>
2014-10-01 21:33:04 +0900

    / ‘demo/rs01_untarLandsat8.R’

--- demo/rs01_untarLandsat8.R
+++ demo/rs01_untarLandsat8.R
@@ -1,5 +1,3 @@
-
+source("./tirSettings.R")
 worklist  <- paste0(hkdMinCloud$ID,".tgz")
-dir.tar  <- "~/Share300sdb/Landsat8/N0/LISTEJapan"
-dir.tif  <- "~/Share500sda/Landsat8/at0_Sensor"
 rs.untar(worklist, fromdir = dir.tar, todir = dir.tif)
