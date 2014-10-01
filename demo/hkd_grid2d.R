44dc6bfc5e5bbb954dd71707dbae151d1ac396ca
Bingwei Tian <bwtian@gmail.com>
2014-10-01 22:14:54 +0900

    / ‘demo/hkd_grid2d.R’

--- demo/hkd_grid2d.R
+++ demo/hkd_grid2d.R
@@ -1,14 +1,6 @@
 #' Make 2D 100m * 100m Grid with lccWgs84 CRS
 #'
-library(sp)
-library(gstat)
-library(plyr)
-library(rgdal)
-library(maptools)
-library(rgeos)
-library(raster)
-
-
+source("./tirSettings.R")
 ## BBOX Raster
 # P  <- Polygon(cbind(c(1152500, 1152500, 1673500, 1673500, 1152500),
 #                     c(1372500, 1843500, 1843500, 1372500, 1372500)))
