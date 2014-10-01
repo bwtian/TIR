8d3a2f52ac99e7dfa5c3250f00c011332bf1ce21
Bingwei Tian <bwtian@gmail.com>
2014-10-01 21:55:55 +0900

    / ‘demo/rs15_tif2png.R’

--- demo/rs15_tif2png.R
+++ demo/rs15_tif2png.R
@@ -1,13 +1,8 @@
-
-source("~/SparkleShare/Rprofile/R/sourceDir.R")
-sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
-sourceDir("~/SparkleShare/TIR/R/")
-#dataDir <- "~/Share500sda/Landsat8/at1_TOA"
+source("./tirSettings.R"
 dataDir  <- dir.toaTbKlcc
 setwd(dataDir)
 r = raster("LC81050292014153LGN00_B10.tif")
 png("test.png")
 plot(r)
 dev.off()
-
 rs.tif2png()
