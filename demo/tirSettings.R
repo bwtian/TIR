140077002e0f08e4984aefc126f8647120bafcc7
Bingwei Tian <bwtian@gmail.com>
2014-10-01 21:29:43 +0900

    / ‘demo/tirSettings.R’

--- demo/tirSettings.R
+++ demo/tirSettings.R
@@ -4,11 +4,14 @@ sourceDir("~/SparkleShare/Rprofile/R/")
 sourceDir("~/SparkleShare/TIR/R/")
 sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
 ### Define Drivers
-driver     <- "~/Share500sda/Landsat8/" # Linux and Windows Symbolink
-dir.tif    <- file.path(driver, "at0_Sensor")
-dir.toaRad <- file.path(driver, "at1_TOA/toaRad")
-dir.toaTbKlccScale  <-  "~/Share500sda/Landsat8/at1_TOA/toaTbKlccScale"
-dir.toaTbKlccScaleMos  <- "~/Share500sda/Landsat8/at1_TOA/toaTbKlccScaleMos"
+#driver     <- "~/Share500sda/Landsat8/" # Linux and Windows Symbolink
+#dir.tar  <- "~/Share300sdb/Landsat8/N0/LISTEJapan"
+dir.tif <- file.path(driver, "at0_Sensor")
+dir.toa <- file.path(driver, "at1_TOA")
+dir.toaRad <- file.path(dir.toa, "toaRad")
+
+dir.toaTbKlccScale  <-  file.path(dir.toa,"toaTbKlccScale")
+dir.toaTbKlccScaleMos  <-  file.path(dir.toa,"toaTbKlccScaleMos")
 
 
 ### Raster Options
