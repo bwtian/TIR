c7a4ce257e9129f32a6d0f5dd845b4eb2b34674f
Bingwei Tian <bwtian@gmail.com>
2014-10-01 22:12:06 +0900

    / ‘demo/tirSettings.R’

--- demo/tirSettings.R
+++ demo/tirSettings.R
@@ -15,15 +15,16 @@ sourceDir("~/SparkleShare/Rprofile/R/")
 sourceDir("~/SparkleShare/TIR/R/")
 sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
 ###  Options
-driver     <- "~/Share500sda/Landsat8/" # Linux and Windows Symbolink
+driver     <- "~/Share500sda/" # Linux and Windows Symbolink
 dir.tmp    <- file.path(driver, "raster_tmp")
 rasterOptions(tmpdir = dir.tmp)
 raster::removeTmpFiles(h = 24)
 gc()
 ### Define Drivers
 #dir.tar  <- file.path(driver, "Landsat8/L1T")
-dir.tif <- file.path(driver, "at0_Sensor")
-dir.toa <- file.path(driver, "at1_TOA")
+dir.sat  <- file.path(dirver, "Landsat8")
+dir.tif <- file.path(dir.sat, "at0_Sensor")
+dir.toa <- file.path(dir.sat, "at1_TOA")
 dir.toaRad <- file.path(dir.toa, "toaRad")
 dir.toaTbKlccScale  <- file.path(dir.toa,"toaTbKlccScale")
 dir.toaTbKlccScaleMos <- file.path(dir.toa,"toaTbKlccScaleMos")
@@ -36,10 +37,10 @@ dir.toaEmi  <-   file.path(dir.toa, "toaEmi")
 dir.toaTbKlcc  <-  file.path(dir.toa,"toaTbKlcc")
 dir.toaTbKlccScale  <-  file.path(dir.toa,"toaTbKlccScale")
 dir.toaTbKlccScaleMos <-  file.path(dir.toa,"toaTbKlccScaleMos")
-dir.surface  <- file.path(driver, "at2_Surface")
+dir.surface  <- file.path(dir.sat, "at2_Surface")
 dir.sufTsKlcc  <-  file.path(dir.surface, "sufTsK")
 dir.database  <- file.path(driver, "at9_Database")
 dir.lulc  <- file.path(dir.database, "LULC")
-
+dir.AG100B  <- "~/Share500sda/AG100B/"
 ### Files
 #hkdmaskb  <- readRDS("~/SparkleShare/TIR/hkdmskb_grdi2d1h.Rds")
