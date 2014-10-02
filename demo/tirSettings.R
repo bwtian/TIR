54fad4204b18013168b570427a4daa6fc6b87ae7
lab1 <bwtian@gmail.com>
2014-10-02 13:39:20 +0900

    / ‘demo/tirSettings.R’

--- demo/tirSettings.R
+++ demo/tirSettings.R
@@ -19,6 +19,7 @@ sourceDir("~/SparkleShare/Rprofile/R/")
 sourceDir("~/SparkleShare/TIR/R/")
 sourceDir("~/SparkleShare/rLandsat8/src/main/R/rLandsat8/R")
 ###  Options
+windowsFonts(Times=windowsFont("TT Times New Roman"))
 driver     <- "~/Share500sda/" # Linux and Windows Symbolink
 dir.tmp    <- file.path(driver, "raster_tmp")
 rasterOptions(tmpdir = dir.tmp)
