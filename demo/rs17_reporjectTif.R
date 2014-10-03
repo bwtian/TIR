bd8752f8c39354b0d774aa61605d8dad5173ba37
t3500 <bwtian@gmail.com>
2014-10-03 15:50:28 +0900

    / ‘demo/rs17_reporjectTif.R’

--- demo/rs17_reporjectTif.R
+++ demo/rs17_reporjectTif.R
@@ -3,6 +3,18 @@
 #' @author Bingwei Tian
 #' @param tif a list of tif files
 #' @date 926
+library(sp)
+library(rgdal)
+library(raster)
+
+source("~/SparkleShare/Rprofile/R/sourceDir.R")
+sourceDir("~/SparkleShare/Rprofile/R/")
+dir.tmp <- "~/Share500sda/Landsat8/raster_tmp"
+rasterOptions(tmpdir = dir.tmp)
+dir.toaTbK  <- "~/Share500sda/Landsat8/at1_TOA/toaTbK/"
+dir.toaTbKlcc  <-  "~/Share500sda/Landsat8/at1_TOA/toaTbKlcc"
+
+toCRS  <- sp::CRS(lccWgs84)
 if (!file.exists(dir.toaTbKlcc)){
         dir.create(dir.toaTbKlcc)
 }
@@ -13,13 +25,11 @@ tif <- list.files(path= dir.toaTbK ,
                   recursive=TRUE,
                   ignore.case=TRUE)
 r.rst  <- lapply(tif, raster)
-hkdmaskb  <- readRDS("~/SparkleShare/TIR/hkdmskb_grdi2d1h.Rds")
 for (i in r.rst) {
         outName  <- paste0(names(i), ".tif")
-        #projectRaster(from = i, crs = toCRS,  method = "ngb",
-        projectRaster(from = i,  to = hkdmaskb,
-                      filename =  file.path(dir.toaTbKlcc, outName),
-                      overwrite=TRUE)
+        projectRaster(i, crs = toCRS,  method = "ngb",
+                      filename =  file.path(dir.toaTbKlcc, outName))
         raster::removeTmpFiles(h = 1) ## Improtant tips for save hardisk
 }
-#test = projectRaster(r.rst[[1]], crs = toCRS, method = "ngb")
+test = projectRaster(r.rst[[1]], crs = lccWgs84, method = "ngb")
+proj4string(r.rst[[1]])
