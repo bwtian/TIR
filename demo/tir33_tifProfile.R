55eaa767e7a1d9a2c5c16cd0ee7ca4205ac60a6f
lab1 <bwtian@gmail.com>
2014-10-06 17:01:50 +0900

    / ‘demo/tir33_tifProfile.R’

--- demo/tir33_tifProfile.R
+++ demo/tir33_tifProfile.R
@@ -1,12 +1,38 @@
 source("~/SparkleShare/TIR/demo/tirSettings.R")
 setwd(dir.toaTbKlccCenterMos)
 mos  <- raster("L8B10CenterMos.tif")
+proj4string(mos)
 ## Decide the point
 point1  <- c(1200000, 1500000)
 point2  <- c(1600000, 1700000)
 ## Connet to Line
-spLine  <-
-plot(r, asp=T)
-## Extract from the raster
-pts = ?data.frame(name=c('A','B','C','D'), lon=c(-1.5,-0.5,0,0.5), lat=c(51,52,53,53.8))
-coordinates(pts) =
+pnts  <- rbind(point1, point2)
+line <- SpatialLines(list(Lines(list(Line(pnts)), "1")))
+proj4string(line)  <- CRS(lccWgs84)
+
+volA  <- readRDS("~/Share500sda//2data/dataProduct/hkd/hkdVol20a_140812_175023.Rds")
+volAlcc  <- spTransform(volA, CRS(lccWgs84))
+
+plot(mos)
+plot(line, col = "blue", add =T)
+plot(volAlcc, pch = 2, size = 6,add =T)
+profile  <- extract(mos, )
+
+ids <- init(mos, v='cell')
+
+## Decide the point
+point1  <- c(1200000, 1500000)
+point2  <- c(1540000, 1680000)
+## Connet to Line
+pnts  <- rbind(point1, point2)
+line <- SpatialLines(list(Lines(list(Line(pnts)), "1")))
+proj4string(line)  <- CRS(lccWgs84)
+pf1  <- extract(mos, line)
+class(pf1)
+# create raster with cell numbers
+idx <- init(mos, v='cell')
+# extract these
+cells <- extract(ids, line)
+# compute xy
+xy = lapply(cells, function(x) xyFromCell(mos, x))
+head(xy)
