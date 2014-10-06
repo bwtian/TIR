9dc70d18bf1f62cc8da28a3f04ddf3c9519ab4fe
t3500 <bwtian@gmail.com>
2014-10-06 14:36:44 +0900

    + ‘demo/tir21_centerTiffs (copy).R’

---
+++ demo/tir21_centerTiffs (copy).R
@@ -0,0 +1,55 @@
+source("~/SparkleShare/TIR/demo/tirSettings.R")
+# tif <- list.files(path= dir.toaTbK ,
+#                   pattern= "B10.tif$",
+#                   all.files=TRUE,
+#                   full.names=TRUE,
+#                   recursive=TRUE,
+#                   ignore.case=TRUE)
+# r.rst  <- lapply(tif, raster)
+# par()
+# par(mfcol =  c(4,4))
+# lapply(r.rst, plot)
+#
+# lapply(r.rst, function(x) plot(x, col = terrain.colors(225)))
+
+
+## lOAD Additional data
+hkdshp  <- "~/Share500sda/2data/dataRaw/japan_ver71/HokkaidoUnion_lccWgs84.shp"
+hkdshp  <- readShapePoly(hkdshp)
+proj4string(hkdshp) <- CRS(lccWgs84)
+hkdmaskb  <- readRDS("~/SparkleShare/TIR/hkdmskb_grdi2d1h.Rds")
+### Raster Stacked
+tif10 <- list.files(path= dir.toaTbKlcc ,
+                  pattern= "B10.tif$",
+                  all.files=TRUE,
+                  full.names=TRUE,
+                  recursive=TRUE,
+                  ignore.case=TRUE)
+r.rst  <- lapply(tif10, raster)
+r.stack  <- stack(r.rst)
+
+r.mask  <- mask(r.stack, hkdmaskb)
+r.merge  <- merge(r.mask)
+plot(r.mask)
+plot(r.merge, col = bpy.colors(255))
+levelplot(r.merge, par.settings =  BuRdTheme)
+## Center values and Merge
+r.center  <- scale(r.mask,center=TRUE, scale=FALSE)
+r.centerMerge  <- merge(r.center)
+plot(r.centerMerge, col = bpy.colors(255))
+plot(r.centerMerge, maxpixels= 1e6, col = oceColorsJet(255))
+levelplot(r.centerMerge,maxpixels=1e6, par.settings =  BuRdTheme)
+volA  <- readRDS("~/Share500sda//2data/dataProduct/hkd/hkdVol20a_140812_175023.Rds")
+volAlcc  <- spTransform(volA, CRS(lccWgs84))
+proj4string(volA)
+plot(volAlcc, pch = 2, size = 6,add =T)
+## Center valuse and Mosaic
+r.center$fun <- mean
+r.centerMos10 <- do.call(mosaic, r.center)
+plot(r.centerMos10, col = oceColorsJet(255))
+plot(volAlcc, pch = 2, size = 6,add =T)
+levelplot(r.centerMerge,maxpixels=1e6, par.settings =  BuRdTheme)
+writeRaster(r.centerMos10, "L8B10CenterMos.tif")
+
+# levelplot(rprob, contour = TRUE, margin = FALSE, at = miat)
+# levelplot(r.stack, col = heat.colors(255), zlim = c(290, 320))
