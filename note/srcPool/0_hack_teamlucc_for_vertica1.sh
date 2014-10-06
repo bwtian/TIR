5784e07d1ddb72a18bb3b16723f500be85b41ab5
t3500 <bwtian@gmail.com>
2014-10-06 14:25:41 +0900

    - ‘note/srcPool/’ - ‘note/srcPool/.org’ - ‘note/srcPool/0_hack_teamlucc_for_vertica1.sh’ - ‘note/srcPool/0_settings.R’ - ‘note/srcPool/10_calc_predictors.R’ - ‘note/srcPool/11_0_build_tif_overviews.R’ - ‘note/srcPool/11_0_copy_training_files.R’ - ‘note/srcPool/11_0_summarize_datafiles.R’ - ‘note/srcPool/11_0_summarize_mosaic_pct_clouds.R’ - ‘note/srcPool/11_0_summarize_trainpixels.R’ ...

--- note/srcPool/0_hack_teamlucc_for_vertica1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /localdisk/home/azvoleff/Code/teamlucc
-
-git pull
-sed -i 's/dplyr/plyr/' ./DESCRIPTION
-sed -i '/dplyr/d' ./NAMESPACE
-R -e 'library(devtools);install(".")'
--- note/srcPool/RStoolbox.R
+++
@@ -1,6 +0,0 @@
-#' RStoolbox: A Collection of Remote Sensing Tools
-#'
-#' @name RStoolbox
-#' @import raster rgeos geosphere plyr randomForest stringr
-#' @docType package
-NULL
