e71877393c5cbee6fefb2fb939e7a7e71c8df9c2
t3500 <bwtian@gmail.com>
2014-10-06 15:44:48 +0900

    / ‘demo/tir33_tifProfile.R’

---
+++ demo/tir33_tifProfile.R
@@ -0,0 +1,12 @@
+source("~/SparkleShare/TIR/demo/tirSettings.R")
+setwd(dir.toaTbKlccCenterMos)
+mos  <- raster("L8B10CenterMos.tif")
+## Decide the point
+point1  <- c(1200000, 1500000)
+point2  <- c(1600000, 1700000)
+## Connet to Line
+spLine  <-
+plot(r, asp=T)
+## Extract from the raster
+pts = ?data.frame(name=c('A','B','C','D'), lon=c(-1.5,-0.5,0,0.5), lat=c(51,52,53,53.8))
+coordinates(pts) =
