666eb7d06e13e5cc8c73f721838055b86a4ff2ee
Bingwei Tian <bwtian@gmail.com>
2014-10-11 12:36:52 +0900

    / ‘R/tir.untar.R’

--- R/tir.untar.R
+++ R/tir.untar.R
@@ -1,8 +1,11 @@
 
-#' Select and untar files in the list form tar dir to tif dir 
+#' Select and untar files in the list form tar dir to tif dir
+#'
+#'
 #' @param list like LC81050292013262LGN00.tgz or LC81050292013262LGN00.tar.gz
 #' @param formdir folders contain the Image usrally the download or database
 #' @param todir  Where the files will copy to, usually the workdir
+#' @export
 rs.untar <- function(list, fromdir, todir){
         if (all(list %in% dir(fromdir))){
                 sapply(list, function(i) {
