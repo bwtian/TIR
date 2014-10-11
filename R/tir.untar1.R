babc20aa27413355f97690f06f8b47aeca47af02
Bingwei Tian <bwtian@gmail.com>
2014-10-11 12:37:09 +0900

    / ‘R/tir.untar1.R’ / ‘R/tir.untarL8.R’

--- R/tir.untar1.R
+++ R/tir.untar1.R
@@ -1,5 +1,5 @@
-
+#' @export
 rs.untar  <- function(list, path = ".", outdir = "../tif", pattern = ".tgz") {
-        tarFiles_v <- list.files(path = path, pattern = pattern, all.files = T, full.names = T) 
+        tarFiles_v <- list.files(path = path, pattern = pattern, all.files = T, full.names = T)
         sapply(tarFiles_v, function(i) {utils::untar(tarfile = i, exdir = file.path(outdir, tools::file_path_sans_ext(basename(i))))})
 }
--- R/tir.untarL8.R
+++ R/tir.untarL8.R
@@ -1,4 +1,5 @@
+#' @export
 tir.untarLandsat  <- function(path = ".", outdir = "../L8_tifs", pattern = ".tgz") {
-        tarFiles_v <- list.files(path = path, pattern = pattern, all.files = T, full.names = T) 
+        tarFiles_v <- list.files(path = path, pattern = pattern, all.files = T, full.names = T)
         sapply(tarFiles_v, function(i) {untar(tarfile = i, exdir = file.path(outdir,tools::file_path_sans_ext(basename(i))))})
 }
