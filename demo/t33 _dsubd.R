6a6bf3e2ccffa169b1d542e0a46140d7209f4bfd
Bingwei Tian <bwtian@gmail.com>
2014-10-13 17:17:08 +0900

    / ‘demo/t33 _dsubd.R’

--- demo/t33 _dsubd.R
+++ demo/t33 _dsubd.R
@@ -12,7 +12,7 @@ dsubd  <- function(data, sub){
 
 }
 source("~/SparkleShare/TIR/demo/tirSettings.R")
-setwd(dir.toaTbKlccCenterMos)
+#setwd(dir.toaTbKlccCenterMos)
 setwd("~/toaTbKlccCenterMos/")
 mos  <- raster("L8B10CenterMos.tif")
 mos.spdf  <- rasterToPoints(mos, spatial=TRUE)
@@ -51,42 +51,63 @@ small  <- function(){
 
 }
 
-x  <- small()
+clipper.l  <- small()
 
+names(clipper.l)  <- c("a", "b", "c", "d")
+str(clipper.l)
+dfs <- lapply(clipper.l, get)
+clipper.df  <- do.call(rbind, clipper.l)
+clipper.df$id  <- as.factor(substr(row.names(clipper.df),1,1))
+summary(clipper.df)
+ggplot(clipper.l[[1]],aes(x,y, fill = tCenter)) + geom_point() +
+facet_wrap(~ id)
+cols = oceColorsJet(10)
+brks  <- seq(-20, 20, 2)
+grobs  <- lapply(clipper.l, function(d) {
+        ggplot(d) +
+        geom_raster(aes(x,y, fill = tCenter)) +
+        scale_x_continuous(label = function(x) x/1000) +
+        scale_y_continuous(label = function(x) x/1000) +
+        xlab("x (km)") +
+        ylab("y (km)") +
+        scale_fill_gradientn(colours = cols,
+                             na.value="white",
+                             breaks = brks,
+                             name = expression(~(degree*C))) +
+                theme_bw(base_size = 12, base_family = "Times") +
+                coord_equal()
 
+        })
 
-
-
-
-
-
-
-set.seed(1011)
-x  <- rnorm(100,mean  =50, sd = 25)
-y  <- rnorm(100,mean  =50, sd = 25)
-data  <- as.data.frame(cbind(x,y))
-data
-### get rectangle map
-sub  <- data.frame(rbind(c(20,60,30,70),
-                         c(80,90,80,90)))
-sub
-names(sub)  <- c("xmin","xmax","ymin","ymax")
-library(ggplot2)
-g1  <- ggplot(data) + geom_point(aes(x =x,y =y)) +
-        geom_rect(data = sub, aes(xmin= xmin, xmax =xmax, ymin = ymin, ymax =ymax),
-                  col ="red", fill = NA)
-
-# subset
-g1 + xlim(80,90) + ylim(80,90)
-g1 + scale_x_continuous(limits = c(80, 90)) + scale_y_continuous(limits = c(80, 90))
-## zoom out
-g1 + coord_cartesian(xlim = c(80,90), ylim = c(80,90))
-# subdata
-out  <- dsubd(data,sub)
-lapply(out,class)
 library(gridExtra)
-gl  <- lapply(out, function(df){
-        ggplot(df) + geom_point(aes(x =x,y =y))
-})
-do.call(grid.arrange, c(gl, list(ncol = 2)))
 
+do.call(grid.arrange, grobs)
+# set.seed(1011)
+# x  <- rnorm(100,mean  =50, sd = 25)
+# y  <- rnorm(100,mean  =50, sd = 25)
+# data  <- as.data.frame(cbind(x,y))
+# data
+# ### get rectangle map
+# sub  <- data.frame(rbind(c(20,60,30,70),
+#                          c(80,90,80,90)))
+# sub
+# names(sub)  <- c("xmin","xmax","ymin","ymax")
+# library(ggplot2)
+# g1  <- ggplot(data) + geom_point(aes(x =x,y =y)) +
+#         geom_rect(data = sub, aes(xmin= xmin, xmax =xmax, ymin = ymin, ymax =ymax),
+#                   col ="red", fill = NA)
+#
+# # subset
+# g1 + xlim(80,90) + ylim(80,90)
+# g1 + scale_x_continuous(limits = c(80, 90)) + scale_y_continuous(limits = c(80, 90))
+# ## zoom out
+# g1 + coord_cartesian(xlim = c(80,90), ylim = c(80,90))
+# # subdata
+# out  <- dsubd(data,sub)
+# lapply(out,class)
+# library(gridExtra)
+# gl  <- lapply(out, function(df){
+#         ggplot(df) + geom_point(aes(x =x,y =y))
+# })
+# do.call(grid.arrange, c(gl, list(ncol = 2)))
+#
