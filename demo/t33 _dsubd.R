c29f2f480293b886c6192dba2c6c28867c9f83e5
Bingwei Tian <bwtian@gmail.com>
2014-10-13 18:44:54 +0900

    / ‘demo/t33 _dsubd.R’

--- demo/t33 _dsubd.R
+++ demo/t33 _dsubd.R
@@ -26,7 +26,7 @@ d  <- as.data.frame(rbind(c(41.92, 140.87),
                           c(43.47, 144.16)))
 names(d)  <- c("lat", "lon")
 dlcc  <- ge.crsTransform(d, lon, lat, xlcc, ylcc, wgs84GRS,lccWgs84)
-rad  <- 5000
+rad  <- 3000
 dlcc$xmin  <- round(dlcc$xlcc, -3) -rad
 dlcc$xmax  <- round(dlcc$xlcc, -3) +rad
 dlcc$ymin  <- round(dlcc$ylcc, -3) -rad
