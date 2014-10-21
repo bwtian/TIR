fb29ca67f5169865afddb2f5ba02fd23794d7eb1
lab1 <bwtian@gmail.com>
2014-10-21 18:13:43 +0900

    / ‘demo/p1402f08_ggLevelTemperature.R’

--- demo/p1402f08_ggLevelTemperature.R
+++ demo/p1402f08_ggLevelTemperature.R
@@ -7,18 +7,22 @@ df  <- subset(hkdKT, Z %% 200 == 0)
 g1 <- ggplot(df) +
 geom_raster(aes(x = X, y =Y, col = Temperatrue)) +
 facet_wrap(~Z)
-g1
+
 g2  <- q1 + scale_x_continuous(label = function(x) x/1000) +
 scale_y_continuous(label = function(x) x/1000) +
 xlab("Easting (km)") +
 ylab("Northing (km)")
-g2
-labelsY = c(min(y), seq(50,500, 50) max(y))
+
+
 y  <- as.numeric(df$Temperatrue)
-labelsY = c(min(y), seq(50,500, 50),max(y))
-breaksY = as.numeric(labelsY)
-g3  <- g2 +  scale_colour_gradientn(name = expression(Temperature~(degree*C)), colours = rev(rainbow(7)), breaks = breaksY, labels = format(breaksY)) +
-theme_bw(base_size = 12, base_family = "Times")
+hist(y)
+breaksY = c(0,100,200,250,300,350,400,max(y))
+labelsY = as.character(breaksY)
+g3  <- g2 +  scale_colour_gradientn(name = expression(Temperature~(degree*C)),
+                                    colours = rev(rainbow(7)),
+                                    breaks = breaksY,
+                                    labels = format(breaksY)) +
+        theme_bw(base_size = 12, base_family = "Times")
 g3
 
 
