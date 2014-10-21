0a9db1a92a6a5a731394222b8f289fae38efdcab
lab1 <bwtian@gmail.com>
2014-10-21 18:15:45 +0900

    / ‘demo/p1402f08_ggLevelTemperature.R’

--- demo/p1402f08_ggLevelTemperature.R
+++ demo/p1402f08_ggLevelTemperature.R
@@ -5,7 +5,7 @@ hkdKT  <- readRDS("hkd/hkd_kt3dlcc_140530_114352.Rds")
 head(hkdKT)
 df  <- subset(hkdKT, Z %% 200 == 0)
 g1 <- ggplot(df) +
-geom_raster(aes(x = X, y =Y, col = Temperatrue)) +
+geom_raster(aes(x = X, y =Y, fill = Temperatrue)) +
 facet_wrap(~Z)
 
 g2  <- g1 + scale_x_continuous(label = function(x) x/1000) +
@@ -18,10 +18,10 @@ y  <- as.numeric(df$Temperatrue)
 hist(y)
 breaksY = c(0,100,200,250,300,350,400,max(y))
 labelsY = as.character(breaksY)
-g3  <- g2 +  scale_colour_gradientn(name = expression(Temperature~(degree*C)),
+g3  <- g2 +  scale_fill_gradientn(name = expression(Temperature~(degree*C)),
                                     colours = rev(rainbow(7)),
                                     breaks = breaksY,
-                                    labels = labelsY +
+                                    labels = labelsY) +
         theme_bw(base_size = 12, base_family = "Times")
 g3
 
