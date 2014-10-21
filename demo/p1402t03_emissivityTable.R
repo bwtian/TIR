14dd9df3c4d04ebc7f612f62ddef8cf4eb72b128
lab1 <bwtian@gmail.com>
2014-10-21 16:42:35 +0900

    / ‘demo/p1402t03_emissivityTable.R’

---
+++ demo/p1402t03_emissivityTable.R
@@ -0,0 +1,7 @@
+Code  <- c(1,2,3,4,5,6,8,10,11)
+LULC  <- c("Water", "Urban", "Paddy", "Crop","Grass", "Deciduous forest",
+           "Evergreen forest", "Bare land", "Snow and ice")
+Color  <- c("darkblue", "red", "skyblue", "yellow", "yellowgreen", "springgreen", "forestgreen", "saddlebrown", "white")
+## emisivity at band 10E
+Emissivity <- c(0.99, 0.93, 0.96, 0.95, 0.96,0.97,0.98,0.93,0.90)
+df  <- cbind(Code,LULC,Emissivity,Color)
